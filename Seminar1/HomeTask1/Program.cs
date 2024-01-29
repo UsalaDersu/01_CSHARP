ed4f96cae8f6c85eed3d7f8352ce17603901e16e
Elena.Mironova <elena.vy.mironova@gmail.com>
2024-01-29 16:25:08 +0300

    Seminar1_HomeTask1

---
+++ Seminar1/HomeTask1/Program.cs
@@ -0,0 +1,14 @@
+﻿int firstNumber = 10;
+int secondNumber = 80;
+if (firstNumber > secondNumber)
+{
+    System.Console.WriteLine("первое число больше");
+}
+else if (firstNumber < secondNumber)
+{
+    System.Console.WriteLine("второе число больше");
+}
+else
+{
+    System.Console.WriteLine(" числа равны");
+}
