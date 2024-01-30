7568a22ed2eb4681bfb1eecb38b10238b3249658
Elena.Mironova <elena.vy.mironova@gmail.com>
2024-01-30 13:19:14 +0300

    seminar1_task2

---
+++ Seminar1/HomeTask2/Program.cs
@@ -0,0 +1,18 @@
+﻿static int FindMax(int a, int b, int c);
+int a = 10;
+int b = 12;
+int c = 13;
+// Введите свое решение ниже
+int result = a;
+
+if (b > result)
+{
+    result = b;
+}
+if (c > result)
+{
+    result = c;
+}
+return result;
+
+
