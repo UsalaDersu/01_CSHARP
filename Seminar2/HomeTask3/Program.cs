0b8317790176a2c492f897627620cb72c80358ec
Elena.Mironova <elena.vy.mironova@gmail.com>
2024-01-31 16:44:37 +0300

    Seminar1_HomeTask3

---
+++ Seminar2/HomeTask3/Program.cs
@@ -0,0 +1,20 @@
+﻿int a = 100;
+int b = a/10;
+int c = a%10;
+
+if (a>=10 & a<=99)
+{
+    if (b>c)
+    {
+        System.Console.WriteLine(b);
+    }
+    else
+    {
+        System.Console.WriteLine(c);
+    }
+}
+else
+{
+    System.Console.WriteLine("введите номер от 10 до 99");
+}
+
