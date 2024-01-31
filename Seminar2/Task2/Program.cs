b2a1dbc8a448d106b1e5ce41f10ec782d3b0f407
Elena.Mironova <elena.vy.mironova@gmail.com>
2024-01-31 12:14:50 +0300

    Seminar2_Task2_second variant

--- Seminar2/Task2/Program.cs
+++ Seminar2/Task2/Program.cs
@@ -1,5 +1,13 @@
 ﻿int a = 487;
 int b = (a % 100) / 10;
 int c = a % 10;
-double result = Math.Pow(b, c);
-System.Console.WriteLine(result);
+//double result = Math.Pow(b, c);
+//System.Console.WriteLine(result);
+
+int d = 1;
+
+for (int i = 1; i <= c; i++)
+{
+    d = d * b;
+}
+System.Console.WriteLine(d);
