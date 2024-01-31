1fd14459bd129de225dab12bb3f87638046be2bb
Elena.Mironova <elena.vy.mironova@gmail.com>
2024-01-31 15:17:50 +0300

    Seminar2_HomeTask1

---
+++ Seminar2/HomeTask1/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 322;
+
+if (a % 7 == 0 & a % 23 == 0)
+{
+    System.Console.WriteLine("Число кратно 7 и 23");
+}
+else
+{
+    System.Console.WriteLine("Число не кратно 7 и 23");
+}
+
+
+
