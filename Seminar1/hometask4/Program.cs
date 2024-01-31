780e579e444d759499e837f10423cd1b4c4c07f7
Elena.Mironova <elena.vy.mironova@gmail.com>
2024-01-31 11:07:57 +0300

    seminar1_hometask4

---
+++ Seminar1/hometask4/Program.cs
@@ -0,0 +1,12 @@
+﻿    int number = 157;
+
+    for (int i = 1; i <= number; i++)
+        if (i % 2 == 0)
+        {
+            System.Console.Write(i);
+            System.Console.Write(' ');
+        }
+
+
+
+
