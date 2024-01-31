33cddffdee2e319d40b5a8769be1010a6a25d63f
Elena.Mironova <elena.vy.mironova@gmail.com>
2024-01-31 12:19:53 +0300

    Seminar2_Task3

---
+++ Seminar2/Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = 4;
+int b = 3;
+if (a % b == 0)
+{
+    System.Console.WriteLine("первое число кратно второму");
+}
+else
+{
+    System.Console.WriteLine("первое число не кратно второму");
+}
