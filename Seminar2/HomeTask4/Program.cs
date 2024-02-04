c13029d0d5325255ce8c5e8c3185c93740816804
Elena.Mironova <elena.vy.mironova@gmail.com>
2024-02-04 22:28:02 +0300

    Seminar2_HomeTask4

---
+++ Seminar2/HomeTask4/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = 81;
+
+string numberString = number.ToString();
+
+Console.WriteLine("Цифры числа по порядку:");
+
+// Перебираем каждый символ (цифру) в строке и выводим его
+for (int i = 0; i < numberString.Length; i++)
+{
+    Console.Write(numberString[i] + " ");
+}
+
+Console.WriteLine(); //
