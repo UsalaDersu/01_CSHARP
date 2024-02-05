55f0a3e64e20b8e74c3ea00a9efc5c9de107ef83
Elena.Mironova <elena.vy.mironova@gmail.com>
2024-02-05 21:50:19 +0300

    lesson2_Task3

---
+++ lesson2/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = 10;
+int[] array = { 5, 5, 5, 7, 1, 35, 1, 3, 1, 5 };
+int i = 0;
+while (i < n)
+{ if (array[i]%2==0)
+{
+    System.Console.Write($"{array[i]} ");
+}
+    i = i + 1;
+}
+ System.Console.WriteLine("нет четных чисел");
