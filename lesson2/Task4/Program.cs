0d94a50e4f80a83c5b859f6b473da8f36f976a8c
Elena.Mironova <elena.vy.mironova@gmail.com>
2024-02-06 14:49:22 +0300

    Lesson2_Task2

---
+++ lesson2/Task4/Program.cs
@@ -0,0 +1,31 @@
+﻿int n = 5;
+int[] array = { 0, 2, 100, 4, 200 };
+int max = array[0];
+//int i = 0;
+
+//while (i < n)
+//{
+//if (array[i] > max)
+//{
+//max = array[i];
+//}
+//i = i + 1;
+//}
+for (int i = 1; i < n; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+}
+System.Console.WriteLine(max);
+
+max = array[0];
+foreach (int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+System.Console.WriteLine(max);
