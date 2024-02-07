e3f99c872b1f1a131b2055be4f9c8a50db138830
Elena.Mironova <elena.vy.mironova@gmail.com>
2024-02-07 14:10:33 +0300

    Seminar3_Task1

---
+++ Seminar3/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] numbers = { -4, 3, 4, 1 };
+
+System.Console.WriteLine("введите число: ");
+int input = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (input == numbers[i])
+    {
+        System.Console.WriteLine("Да");
+     return;
+}
+}  
+System.Console.WriteLine("Нет");
