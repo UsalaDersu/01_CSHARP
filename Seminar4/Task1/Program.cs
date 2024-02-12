08113646addf073a17023dd918a97c4250b0cc1e
Elena.Mironova <elena.vy.mironova@gmail.com>
2024-02-12 14:30:51 +0300

    Seminar4_Task1

---
+++ Seminar4/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿int Sum(int a, int b, int c)
+{
+    int res = a + b + c;
+    return res;
+}
+
+int amount(int a, int b, int c)
+{
+    int res = a * b * c;
+    return res;
+}
+
+
+System.Console.WriteLine(Sum(5, 7, 8));
+System.Console.WriteLine(amount(5, 7, 8));
