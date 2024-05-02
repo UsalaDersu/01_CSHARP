71ddfa2da3c6770773cd1b192d1a47cc0733a557
Elena.Mironova <elena.vy.mironova@gmail.com>
2024-05-02 17:26:04 +0300

    Seminar6Task3

---
+++ Seminar6/Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+Console.WriteLine("Введите строку для проверки на палиндром:");
+string inputString = Console.ReadLine();
+
+string reversedString = ReverseString(inputString);
+bool isPalindrome = (inputString == reversedString);
+
+Console.WriteLine($"Исходная строка: {inputString}");
+Console.WriteLine($"Перевернутая строка: {reversedString}");
+Console.WriteLine($"Является ли палиндромом? {(isPalindrome ? "Да" : "Нет")}");
+
+// Функция для переворота строки
+string ReverseString(string input)
+{
+    char[] charArray = input.ToCharArray(); // Преобразуем строку в массив символов
+    Array.Reverse(charArray); // Разворачиваем массив символов
+    return new string(charArray); // Преобразуем массив символов обратно в строку
+}
