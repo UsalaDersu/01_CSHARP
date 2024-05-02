99f3a9b2ce337b4f493315306e27ad185ad74db6
Elena.Mironova <elena.vy.mironova@gmail.com>
2024-05-02 16:24:24 +0300

    Seminar6_Task1

---
+++ Seminar6/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+char[,] charArray = {
+            {'a', 'b', 'c'},
+            {'d', 'e', 'f'}
+        };
+string resultString = "";
+
+for (int i = 0; i < charArray.GetLength(0); i++)
+{
+    for (int j = 0; j < charArray.GetLength(1); j++)
+    {
+        resultString += charArray[i, j]; // Добавляем символ к строке
+    }
+}
+Console.WriteLine(resultString);
