fe6a447fcc4a68d36f668d38a86c9b70d6b8bcc9
Elena.Mironova <elena.vy.mironova@gmail.com>
2024-05-02 13:47:59 +0300

    Task1

---
+++ lesson3/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// double CalculateFormula(int a, int b, int c, int d)
+// {
+//     double numenator = a * b;
+//     int denomenator = c + d;
+//     double result = numenator / denomenator;
+//     return result;
+// }
+
+//     double result = CalculateFormula(1, 2, 3, 4);
+//     System.Console.WriteLine(result);
+
+float CalculateFormula(int a, int b, int c, int d) //double - число с точкой ( нецелое), int -только целое
+{
+float numenator = a * b;
+int denomenator = c + d;
+float result = numenator / denomenator;
+return result;
+}
+float result = CalculateFormula(1, 2, 3, 4);
+System.Console.WriteLine(result);
