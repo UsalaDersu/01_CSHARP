5a663824c711a77c6803d8f503c3c4d37d672129
Elena.Mironova <elena.vy.mironova@gmail.com>
2024-05-02 15:11:31 +0300

    new in task2

--- lesson3/Task2/Program.cs
+++ lesson3/Task2/Program.cs
@@ -1,12 +1,13 @@
-﻿void PrintSquares(int n)
+﻿// Задача - вывод на экран квадрата чисел
+void PrintSquares(int n) // название функции - печать квадрата.void означает, что она ничего не возвращает. Функция просто считает.
 {
-    int i = 1;
+    int i = 1; //
     while (i <= n)
     {
         System.Console.Write($"{i * i} ");
-        i++;
+        i++;// операция ++ заменяет = i+1
     }
 }
 PrintSquares(5);
-PrintSquares(10);
-PrintSquares(15);
+//PrintSquares(10);
+//PrintSquares(15);
